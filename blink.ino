4974a44d066b0b13f17b614f1794c252854ae76f
Gorka <gsanz10@hotmail.com>
2018-09-25 16:49:07 +0200

    blink.ino

--- blink.ino
+++ blink.ino
@@ -1,10 +1,10 @@
-void setups() {
+void setup() {
   pinMode(LED_BUILTIN, INPUT);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, LOW);
-  sleep(1000);
+  delay(1000);
   digitalRead(LED_BUILTIN, HIGH);
-  sleep(2000);
+  delay(2000);
 }
